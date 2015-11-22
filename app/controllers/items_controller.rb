288da3ada0cffad2f9ed40c6d8acac93b68c3652
shinichi <mactwist1007@kuh.biglobe.ne.jp>
2015-11-22 14:13:46 +0900

    new追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,21 @@
 class ItemsController < ApplicationController
+
+  def create
+    @item=Item.new(item_params)
+    @item.save
+    redirect_to "/items/#{@item.id}"
+  end
+
+  def item_prams
+    params.require(:item).permit(:name, :price, :seller_id, :description, :email, :image_url)
+  end
   def show
     @item=Item.find(params[:id])
   end
   def index
     @items=Item.all
+  end
+  def new
+    @item=Item.new
   end
 end
